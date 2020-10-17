78c1fa1013bcfb636962fcf3c55d30e24a878445
Tilek <43582297+cherikbaev13@users.noreply.github.com>
2020-10-17 11:25:34 +0600

    add model User

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+
+        public int ID { get; set; }
+        [Display(Name = "Логин")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
