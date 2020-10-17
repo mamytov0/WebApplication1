2c60ce1a45d3cac9c9be4c700423d02e7dde5364
nursultan mamytov <nu40005@gmail.com>
2020-10-17 11:42:29 +0600

    Adding Login Model

---
+++ WebApplication1/Utility/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Utility
+{
+    public class ConnectionString
+    {
+
+        private static string cName = "Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =localhost)(PORT = 1521 ))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME =orcl.local.com )));Password= 123 ;User ID=tilek;";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
