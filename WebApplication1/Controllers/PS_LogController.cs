43b32d11fdbbf2406a9b3971c4780ed95d991b5f
Tilek <43582297+cherikbaev13@users.noreply.github.com>
2020-10-31 10:57:30 +0600

    Adding Controller for Logs and Data access

---
+++ WebApplication1/Controllers/PS_LogController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PS_LogController : Controller
+    {
+        LogAccessLayer logAccess = null;
+
+        public PS_LogController(LogAccessLayer l)
+        {
+            logAccess = l;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.start = "";
+            ViewBag.finish = "";
+            List<PS_Log> logs = new List<PS_Log>();
+            return View(logs);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(DateTime Date_start, DateTime Date_finish)
+        {
+            ViewBag.Date_start = Date_start;
+            ViewBag.Date_finish = Date_finish;
+            List<PS_Log> log = logAccess.Get_Log(Date_start, Date_finish);
+            return View(log);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public FileResult GetFile(DateTime start, DateTime finish)
+        {
+            ViewBag.start1 = start;
+            ViewBag.finish1 = finish;
+            List<PS_Log> lis = logAccess.Get_Log(start, finish);
+            Report rep = new Report();
+            return rep.GetReportBy(lis);
+        }
+    }
+}
+}
---
+++ WebApplication1/StoredProcedure/LogAccessLayer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.StoredProcedure
+{
+    public class LogAccessLayer
+    {
+        string connectionString;
+
+        public LogAccessLayer(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+        public List<PS_Log> Get_Log(DateTime start, DateTime finish)
+        {
+            //string connectionString = ConnectionString.CName;
+            List<PS_Log> log_list = new List<PS_Log>();
+
+
+            using (OracleConnection con = new OracleConnection(connectionString))
+            {
+                OracleCommand cmd = new OracleCommand("GET_LOG", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("PARAM1", OracleDbType.RefCursor, System.Data.ParameterDirection.Output);
+                cmd.Parameters.Add("Date_start", OracleDbType.Date, start, System.Data.ParameterDirection.Input);
+                cmd.Parameters.Add("Date_finish ", OracleDbType.Date, finish, System.Data.ParameterDirection.Input);
+                con.Open();
+                OracleDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    PS_Log log = new PS_Log();
+                    log.System_Req = (rdr["SYSTEM_REQ"]).ToString();
+                    log.Request = (rdr["REQUEST"]).ToString();
+                    log.Response = (rdr["RESPONSE"]).ToString();
+                    log.Target_Path = (rdr["TARGET_PATH"]).ToString();
+                    log.INS_Date = Convert.ToDateTime(rdr["INS_DATE"]);
+                    if (rdr["AMNT"] == null)
+                    {
+                        log.AMNT = 0;
+                    }
+                    else
+                    {
+                        log.AMNT = Convert.ToInt32(rdr["AMNT"]);
+                    }
+
+                    log.ACC = (rdr["ACC"]).ToString();
+                    log.Cur = (rdr["CUR"]).ToString();
+                    log_list.Add(log);
+                }
+            }
+            return log_list;
+        }
+    }
+}
