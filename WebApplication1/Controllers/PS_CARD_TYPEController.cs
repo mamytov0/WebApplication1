faf6915e58373e14f3cc740b123e275b630c7955
nursultan mamytov <nu40005@gmail.com>
2020-10-31 10:29:17 +0600

    Подправил котролер тип карты

--- WebApplication1/Controllers/PS_CARD_TYPEController.cs
+++ WebApplication1/Controllers/PS_CARD_TYPEController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using WebApplication1.StoredProcedure;
 
 namespace WebApplication1.Controllers
 {
