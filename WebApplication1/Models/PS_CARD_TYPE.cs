4bf0564dd7a306540149bf450614ce1485d7e9cd
nursultan mamytov <nu40005@gmail.com>
2020-10-31 10:21:48 +0600

    Добавление модельки для карты

---
+++ WebApplication1/Models/PS_CARD_TYPE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public partial class PS_CARD_TYPE
+    {
+        public int ID { get; set; }
+        [Display(Name = "Наименование")]
+        public string NAME { get; set; }
+        public string FIRSTSYMBOLS { get; set; }
+        [Display(Name = "Адрес Сервиса")]
+        public string SERVICEADRESS { get; set; }
+        [Display(Name = "Логин")]
+        public string LOGIN { get; set; }
+        [Display(Name = "Пароль")]
+        public string PASS { get; set; }
+    }
+}
