a1128150c582c2a1a12f0460fb7185b1b35d3820
Tilek <43582297+cherikbaev13@users.noreply.github.com>
2020-11-13 22:43:59 +0600

    Addin Common Purpose model and validation of this model

---
+++ WebApplication1/Models/PS_Common_purpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PS_Common_purpose
+    {
+        public int ID { get; set; }
+        [Display(Name = "Код Сервиса")]
+        public string Code_for_service { get; set; }
+        [Display(Name = "Наименование")]
+        public string Name { get; set; }
+        [Display(Name = "Описание")]
+        public string Decription { get; set; }
+        [Display(Name = "Тип Карты")]
+        public int Typeof_Card { get; set; }
+
+        public string Card;
+        public Type_Card Type_ { get; set; }
+    }
+}
