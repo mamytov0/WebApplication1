39e94d0f39eecdcf2549221deefa97d995f98578
Tilek <43582297+cherikbaev13@users.noreply.github.com>
2020-11-13 22:45:53 +0600

    Addin Service Purpose model and validation of this model

---
+++ WebApplication1/Models/PS_Service_Purpose.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PS_Service_Purpose
+    {
+        public int ID { get; set; }
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+        [Display(Name = "Код Сервиса")]
+        public string Code_for_Service { get; set; }
+        public int Card_type { get; set; }
+
+        public string Card;
+
+    }
+}
