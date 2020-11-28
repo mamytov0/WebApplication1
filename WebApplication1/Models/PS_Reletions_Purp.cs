d58a08dc2144f5c3c35af0138689540095843fab
nursultan mamytov <nu40005@gmail.com>
2020-11-28 10:22:06 +0600

    Добавление класса для Reletions

---
+++ WebApplication1/Models/PS_Reletions_Purp.cs
@@ -0,0 +1,24 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paid_System_PS_.Models
+{
+    public partial class PS_Reletions_Purp
+    {
+        public int ID { get; set; }
+        [Display(Name = "Сервис назначения")]
+        public int Service_Purp { get; set; }
+        [Display(Name = "Общее назначение")]
+        public int Common_Serv { get; set; }
+        
+        public string service_p;
+        
+        public string common_s;
+
+
+    }
+}
