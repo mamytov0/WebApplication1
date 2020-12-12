5122f3e62bbd287bfca68f4d61209e5a971dc7a3
Tilek <43582297+cherikbaev13@users.noreply.github.com>
2020-12-12 09:36:36 +0600

    Addin deleting methods

--- WebApplication1/Controllers/PS_Service_PurposeController.cs
+++ WebApplication1/Controllers/PS_Service_PurposeController.cs
@@ -60,5 +60,70 @@ namespace WebApplication1.Controllers
             return View(pS_Service_);
         }
         [Authorize]
-    
+        public ActionResult Delete(int ID)
+        {
+            PS_Service_Purpose pS_Service_ = _PurposeDataAccessLayer.Details(ID);
+
+            return View(pS_Service_);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(PS_Service_Purpose pS_Service)
+        {
+            try
+            {
+                // TODO: Add delete logic here  
+                _PurposeDataAccessLayer.Delete(pS_Service.ID);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [Authorize]
+        public ActionResult Edit(int ID)
+        {
+            IEnumerable<PS_CARD_TYPE> ct = pS_CARD_TYPE_DataAccessLayer.GetAllData();
+
+            PS_Service_Purpose pS = _PurposeDataAccessLayer.Details(ID);
+
+            //ViewData["Card"] = new SelectList(tp, "ID", "Name", Cart.Typeof_Card);
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            foreach (PS_CARD_TYPE dr in ct)
+            {
+
+                if (pS.Card == dr.NAME)
+                {
+                    list.Add(new SelectListItem { Text = dr.NAME.ToString(), Value = dr.ID.ToString(), Selected = true });
+                }
+                else
+                {
+                    list.Add(new SelectListItem { Text = dr.NAME.ToString(), Value = dr.ID.ToString(), Selected = false });
+                }
+
+            }
+
+            ViewBag.Card_type = list;
+
+            return View(pS);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PS_Service_Purpose s_Service_Purpose)
+        {
+            try
+            {
+                // TODO: Add update logic here  
+                _PurposeDataAccessLayer.Update(s_Service_Purpose);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
 }
